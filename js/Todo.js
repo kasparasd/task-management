3c8e52756a2a0fb173f07f349531207aaad32f49
Kasparas <kasparas.dapsys@gmail.com>
2023-11-09 08:24:19 +0200

    todo

--- js/Todo.js
+++ js/Todo.js
@@ -1,43 +1,82 @@
-export class Todo { 
+export class Todo {
     constructor(selector, columns) {
         this.selector = selector;
         this.columns = columns;
         this.DOM = null;
+        this.columnsDOM = [];
+        this.tasks = [];
+        this.lastUsedtaskId = 0;
 
         this.init();
     }
 
     init() {
-        this.updateDOMelement();
+        const err = this.updateDOMelement();
+        if (typeof err === 'string') {
+            return console.error(err);
+        }
+
         this.render();
     }
 
     updateDOMelement() {
-        if(typeof this.selector!== 'string' || this.selector === '') {
-            throw new Error('Netinkamas selektorius');
+        if (typeof this.selector !== 'string' || this.selector === '') {
+            return 'Netinkamas selektorius';
         }
-        this.DOM = document.querySelector(this.selector)
+
+        this.DOM = document.querySelector(this.selector);
     }
 
     render() {
         let HTML = '';
 
-        for (const column of this.columns){
+        for (const column of this.columns) {
             HTML += `
-                <div>
-                    <h2>${column}</h2>
-                    <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    </ul>
-                </div>
-            `
+                <div class="column">
+                    <h2 class="title">${column}</h2>
+                    <ul class="task-list"></ul>
+                </div>`;
         }
+
         this.DOM.classList.add('todo');
         this.DOM.innerHTML = HTML;
-        this.DOM.style.gridTemplateColumns = `repeat(${this.columns.length}, 1fr)`
+        this.DOM.style.gridTemplateColumns = `repeat(${this.columns.length}, 1fr)`;
+
+        this.columnsDOM = this.DOM.querySelectorAll('.task-list');
     }
- }
+
+    addTask(task) {
+        this.tasks.push({
+            ...task,
+            isDeleted: false,
+        });
+        const taskID = ++this.lastUsedtaskId;
+        let tagsHTML = '';
+
+        for (const tag of task.tags) {
+            tagsHTML += `<div class="tag" style="color: ${tag.color};">${tag.text}</div>`;
+        }
+
+        const HTML = `
+            <li id="task_${taskID}" class="task-card">
+                <div class="task-actions">
+                    <button class="fa fa-trash"></button>
+                </div>
+                <div class="task-title">${task.title}</div>
+                <div class="task-desc">${task.desc}</div>
+                <div class="task-tags">${tagsHTML}</div>
+                <div class="task-deadline">${task.deadline}</div>
+            </li>`;
+
+        // this.columnsDOM[task.columnIndex].innerHTML += HTML;
+        this.columnsDOM[task.columnIndex].insertAdjacentHTML('beforeend', HTML);
+
+        const taskDOM = document.getElementById(`task_${taskID}`);
+        const deleteButtonDOM = taskDOM.querySelector('.fa-trash');
+
+        deleteButtonDOM.addEventListener('click', () => {
+            this.tasks[taskID - 1].isDeleted = true;
+            taskDOM.remove();
+        });
+    }
+}
--- js/main.js
+++ js/main.js
@@ -2,6 +2,38 @@ import { Todo } from "./Todo.js";
 
 const kanban = new Todo('#kanban',['Back log','Todo', 'In progress', 'Done']);
 
-console.log(kanban);
+kanban.addTask({
+    columnIndex: 1,
+    title: 'Pirmos uzduoties antraste',
+    desc: 'Pirmos uzdoties pilnas aprasas ... ilgas aprasas labai ilgas tekstas, issamiai nupasakoja ka reikia nuveikti',
+    createdOn: '2023-11-08 09:03:15',
+    deadline: '2023-12-24 00:00:00',
+    tags: [
+        {text: 'Design', color: '#333'},
+    ],
+})
 
-kanban.addtask('')
+kanban.addTask({
+    columnIndex: 0,
+    title: 'Antra uzduotis',
+    desc: 'Antros uzdoties pilnas aprasas ... ilgas aprasas labai ilgas tekstas, issamiai nupasakoja ka reikia nuveikti',
+    createdOn: '2023-11-08 10:03:15',
+    deadline: '2023-12-24 00:00:00',
+    tags: [
+        {text: 'UX', color: '#f00'},
+        {text: 'UI', color: '#090'},
+    ],
+})
+
+kanban.addTask({
+    columnIndex: 0,
+    title: 'Trecia uzduotis',
+    desc: 'Trecios uzdoties pilnas aprasas ... ilgas aprasas labai ilgas tekstas, issamiai nupasakoja ka reikia nuveikti',
+    createdOn: '2023-11-08 11:03:15',
+    deadline: '2023-12-20 00:00:00',
+    tags: [
+        {text: 'Development', color: '#00c'},
+    ],
+})
+
+console.log(kanban);
